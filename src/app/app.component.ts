5b9dcae2e3f68f6bd85b8686c0c294f2943ed566
Mahmoud Ibrahim Mohamed <jfijcc124@gmail.com>
2025-04-19 12:21:59 +0200

    Initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClient,
+  HttpClientModule,
+} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DatingAppUI';
+
+  /**
+   *
+   */
+  constructor(private _httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this._httpClient.get('https://localhost:7120/api/Account/users').subscribe({
+      next: (response) => {
+        console.log(response);
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {
+        console.log('Request Completed');
+      },
+    });
+  }
 }
